b9c6e4ae2f062d1461217ab60cec2322d0269030
徐晟峰 <shengfeng.xu@shanbay.com>
2018-07-12 13:47:16 +0800

    installed eslint and prettier

--- fittings/cors.js
+++ fittings/cors.js
@@ -6,12 +6,11 @@ var CORS = require('cors');
 // config options: https://www.npmjs.com/package/cors
 
 module.exports = function create(fittingDef, bagpipes) {
+    debug('config: %j', fittingDef);
+    var middleware = CORS(fittingDef);
 
-  debug('config: %j', fittingDef);
-  var middleware = CORS(fittingDef);
-
-  return function cors(context, cb) {
-    debug('exec');
-    middleware(context.request, context.response, cb);
-  }
+    return function cors(context, cb) {
+        debug('exec');
+        middleware(context.request, context.response, cb);
+    };
 };
--- fittings/express_compatibility.js
+++ fittings/express_compatibility.js
@@ -1,45 +1,48 @@
 'use strict';
 
-var debug = require('debug')('swagger:cors');
-var Url = require('url');
+const debug = require('debug')('swagger:cors');
+const Url = require('url');
 
 module.exports = function create(fittingDef, bagpipes) {
+    debug('config: %j', fittingDef);
 
-  debug('config: %j', fittingDef);
-
-  return function express_compatibility(context, cb) {
-    debug('exec');
-    expressCompatibility(context.request, context.response, cb);
-  }
+    return function express_compatibility(context, cb) {
+        debug('exec');
+        expressCompatibility(context.request, context.response, cb);
+    };
 };
 
 function expressCompatibility(req, res, next) {
+    if (!req.query || !req.path) {
+        const url = Url.parse(req.url, !req.query);
+        req.path = url.path;
+        req.query = url.query;
+    }
 
-  if (!req.query || !req.path) {
-    var url = Url.parse(req.url, !req.query);
-    req.path = url.path;
-    req.query = url.query;
-  }
+    if (!res.json) {
+        res.json = function(obj) {
+            res.statusCode = res.statusCode || 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(obj));
+        };
+    }
 
-  if (!res.json) {
-    res.json = function(obj) {
-      res.statusCode = res.statusCode || 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(obj));
-    };
-  }
+    if (!req.get)
+        req.get = function(name) {
+            return this.headers[name];
+        };
 
-  if (!req.get) req.get = function(name) {
-    return this.headers[name];
-  };
+    if (!res.set) {
+        res.set = res.setHeader;
+    }
+    if (!res.get) {
+        res.get = res.getHeader;
+    }
+    if (!res.status) {
+        res.status = function(status) {
+            res.statusCode = status;
+        };
+    }
 
-  if (!res.set) { res.set = res.setHeader; }
-  if (!res.get) { res.get = res.getHeader; }
-  if (!res.status) {
-    res.status = function(status) {
-      res.statusCode = status;
-    };
-  }
-
-  next();
+    next();
 }
